412cf5cc6b838d0a3a9e02ecb6e5397751178d9e
Ismail <ismailsmakhus@gmail.com>
2021-07-22 14:56:45 +0700

    add dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
+import dotenv from 'dotenv';
 import express from 'express';
 
+dotenv.config();
+
 const app = express();
-const port = 3000;
+const port = process.env.APP_PORT;
 
 app.get('/', (req, res) => {
-    res.send('Hello world');
+    res.send(`Hello from ${process.env.APP_NAME}`);
 });
 
 app.listen(port, () => console.log(`server running on port ${port}`));
