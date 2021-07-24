566b70672f1261ccaa885dce87cb1127302f2ef3
Ismail <ismailsmakhus@gmail.com>
2021-07-24 21:43:11 +0700

    add api routes

--- src/server.ts
+++ src/server.ts
@@ -5,13 +5,23 @@ import express from 'express';
 dotenv.config();
 
 const app = express();
-const port = process.env.APP_PORT;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// api routes
+var api = express.Router();
+api.get('/', (req, res) => {
+    res.send({ 'hello': 'hai' });
+});
+
+app.use('/api', api);
+
+// web routes
 app.get('/', (req, res) => {
     res.render('index');
 });
 
+// run server
+const port = process.env.APP_PORT;
 app.listen(port, () => console.log(`server running on port ${port}`));
