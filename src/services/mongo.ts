9c5953dee88e8ab3b70d8f0638d7616a3906b3d0
Ismail <ismailsmakhus@gmail.com>
2022-02-23 21:54:35 +0700

    update

--- src/services/mongo.ts
+++ src/services/mongo.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
 export async function init() {
-    const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/cumacuma';
+    const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/dbname';
     await mongoose.connect(uri).catch(err => console.log(err));
 }
